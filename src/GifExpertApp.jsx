385fbf558b849a62c6fe66ff0a1065fcd68710a5
Luis Angel <angelfgdeveloper@gmail.com>
2023-02-12 18:00:39 -0600

    Add estilos

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,14 @@
 export const GifExpertApp = () => {
   return (
     <>
+      {/* Titulo */}
+
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Items (Gifs) */}
+              {/* Gift Item */}
     </>
   );
 }
