61c5923530310cfe7eab0ceb963f224bfa6d69f1
Luis Angel <angelfgdeveloper@gmail.com>
2023-02-12 18:56:47 -0600

    Creacion de componente, uso de eventos y comunicacion de los mismos

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+
+  const [ categories, setCategories ] = useState([
+    'One Punch',
+    'Dragon Ball'
+  ]);
+
+  const onAddCategory = () => {
+    setCategories( ['Valorant', ...categories] );
+    // setCategories( cat => [ ...cat, 'Valorant' ]);
+  }
+
   return (
     <>
       {/* Titulo */}
@@ -7,8 +21,18 @@ export const GifExpertApp = () => {
       <h1>GifExpertApp</h1>
 
       {/* Input */}
+      <AddCategory setCategories={ setCategories } />
 
       {/* Listado de Items (Gifs) */}
+      {/* <button onClick={ onAddCategory }>Agregar</button> */}
+      <ol>
+        { 
+          categories.map( category => {
+            return <li key={ category }>{ category }</li>;
+          })
+        }
+        {/* <li>ABC</li> */}
+      </ol>
               {/* Gift Item */}
     </>
   );
