560640632ab99fc1956ceee8771b24e0d150ffc4
Luis Angel <angelfgdeveloper@gmail.com>
2023-02-15 19:13:49 -0600

    Uso de useEffect, build de produccion, creacion de componente card, className y uso de ...rest

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
+import { GifItem } from './GifItem';
 
 export const GifGrid = ({ category }) => {
 
+  const [images, setImages] = useState([]);
+
+  const getImages = async() => {
+    const newImages = await getGifs( category );
+    setImages(newImages);
+  }
+
+  // useEffect (un hook) => Sirve para disparar efectos secundarios
+  // No se vuelve a ejecturar el fetch 
   useEffect(() => {
-    getGifs(category);
+    // #1 forma
+    // getGifs(category).then(newimages => setImages(newimages));
+    // #2 forma
+    getImages();
   }, [])
 
   return (
     <>
       <h3>{ category }</h3>
+
+      <div className='card-grid'>
+        {
+          images.map(( image ) =>( 
+            <GifItem 
+              key={ image.id } 
+              { ...image } /> 
+          ))
+        }
+      </div>
+
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './styles.css';
 import { GifExpertApp } from './GifExpertApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // StrictMode => ayuda componentes que tiene problemas o warnings
   <React.StrictMode>
     <GifExpertApp />
   </React.StrictMode>,
